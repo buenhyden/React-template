97fe9dbe37a6baa13ca93e252f33a3b24bfe0191
hyunyoun <chochyjj@gmail.com>
2025-11-24 12:47:12 +0900

    파일 추가 - Dockerfile - docker-compose.yml - eslint.config.js - nginx.conf - postcss.config.js - tailwind.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
